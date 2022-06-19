1bcb02fdf17111dabe5bfd3c3db64357edb75138
Antonzlo <43794515+Antonzlo@users.noreply.github.com>
2022-06-20 00:49:22 +0300

    findPort

--- lovense.js
+++ lovense.js
@@ -1,4 +1,4 @@
-async function getToys(ip = '127-0-0-1', port=30010) {
+async function getToys(ip = '127-0-0-1', port = 30010) {
     return fetch('https://'+ip+'.lovense.club:'+port+'/GetToys').then(async e => {
 		e = await e.json();
 	    window.toys = JSON.parse(e.data.toys);
@@ -12,9 +12,23 @@ function parseToys(toys) {
     ).join('<br>');
 }
 
-async function run(ip = '127-0-0-1', port=30010, cmd = { command:"Function", action:'vibrate:0', timeSec:0, apiVer:1 }) {
+async function run(ip = '127-0-0-1', port = 30010, cmd = { command:"Function", action:'vibrate:0', timeSec:0, apiVer:1 }) {
     return fetch('https://'+ip+'.lovense.club:'+port+'/command',{
 	    method: 'post',
 	    body: JSON.stringify(cmd)
     }).then(e=>e.json());
+}
+
+async function findPort(ip = '127-0-0-1') {
+    let port = 30010, out = 0;
+    do {
+        await fetch('https://'+ip+'.lovense.club:'+port+'/GetToys', { timeout:3000 })
+        .then(async e => {
+            out = port;
+            e = await e.json();
+	        window.toys = JSON.parse(e.data.toys);
+            break;
+        }).catch(()=>{});
+    } while (port++ < 30013)
+    return out || false;
 }
