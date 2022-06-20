a236f21809523c47c1a21e3c69fadfd2c677c996
Antonzlo <43794515+Antonzlo@users.noreply.github.com>
2022-06-20 23:32:09 +0300

    N

--- lovense.js
+++ lovense.js
@@ -22,7 +22,7 @@ async function getToys(ip = '127-0-0-1', port = 30010) {
 
 function parseToys(toys) {
     return Object.keys(toys).map(x=>
-        `${toys[x].name}${toys[x].nickname ? ` (${toys[x].nickname})` : ''} - ${toys[x].battery}%`
+        `${toys[x].name}${toys[x].nickName ? ` (${toys[x].nickName})` : ''} - ${toys[x].battery}%`
     ).join('<br>');
 }
 
